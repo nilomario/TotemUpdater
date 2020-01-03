44d4dcd9af9626f7d213fd1741dcd3d090d81828
unknown <ntrujillo.reina@gmail.com>
2020-01-02 23:01:33 -0300

    First Commit

---
+++ Passline.Totem.Web/Scripts/AdminLte/adminLte.js
@@ -0,0 +1,20 @@
+﻿var divDetailsProduct;
+
+$(function () {
+    InicializarVariablesItem();
+    EnlazarEventosItem();
+});
+
+function InicializarVariablesItem() {
+    divListGrid = $("#dataTable");
+    divCreateItem = $("#modalCreateItem");
+    idFormCreateItem = "#formCreateItem";
+    divEditItem = $("#modalEditItem");
+    idFormEditItem = "#formEditItem";
+    divDetailsProduct = $("#modalDetailsProduct");
+    idFormDetailsItem = "#formDetailstem";
+}
+
+function EnlazarEventosItem() {
+
+}
---
+++ github.bat
@@ -0,0 +1,5 @@
+c:
+cd c:\inetpub\wwwroot
+git pull https://github.com/nilomario/enel masterc:
+cd c:\inetpub\wwwroot
+git pull https://github.com/nilomario/enel master
